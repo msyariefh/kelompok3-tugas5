1c4f95cfa7f996bb62332fc86d8711d541184a57
HiDE <msyariefhidayatulloh14@gmail.com>
2022-08-24 13:28:01 +0800

    Add Main Menu module
    - Add Main menu module
    - Create a main menu prefab
    - Change Event Listener from inspector to script

---
+++ Assets/Scripts/Main Menu/MainMenu.cs
@@ -0,0 +1,42 @@
+using UnityEngine.SceneManagement;
+using TankU.Audio;
+using UnityEngine.UI;
+using UnityEngine;
+
+namespace TankU.MainMenu
+{
+    public class MainMenu : MonoBehaviour
+    {
+        [SerializeField] private Button _playButton;
+        [SerializeField] private Button _settingsButton;
+        [SerializeField] private Button _exitButton;
+        [SerializeField] private GameObject _settingUI;
+
+        private void Awake()
+        {
+            _playButton.onClick.AddListener(OnPlayButtonClicked);
+            _settingsButton.onClick.AddListener(OnSettingsButtonClicked);
+            _exitButton.onClick.AddListener(OnExitButtonClicked);
+        }
+        private void Start()
+        {
+            AudioManager.Instance.PlayBGM("MainMenuBGM");
+        }
+
+        private void OnPlayButtonClicked()
+        {
+            // Change to gameplay scene
+        }
+
+        private void OnSettingsButtonClicked()
+        {
+            _settingUI.SetActive(true);
+        }
+
+        private void OnExitButtonClicked()
+        {
+            Application.Quit();
+        }
+    }
+
+}
