58f3dbe71dd6dd295d458bb7661a571dd8ff03f1
belial66622 <killbill.panjaitan@gmail.com>
2022-08-25 15:32:39 +0800

    Add GameOver

---
+++ Assets/Scripts/GameOver/GameOver.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+namespace TankU.GameOver
+{
+    public class GameOver : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI _winner;
+        [SerializeField] private Button MainMenu, Retry;
+    //    [SerializeField] private HPController _hpController;
+
+        private void Start()
+        {
+            MainMenu.onClick.RemoveAllListeners();
+            Retry.onClick.RemoveAllListeners();
+            MainMenu.onClick.AddListener(GoMainMenu);
+            Retry.onClick.AddListener(GoRetry);
+        }
+
+        private void OnEnable()
+        {
+  //          _hpController.OnGameOver += OnGameOver;   
+        }
+
+        private void OnDisable()
+        {
+    //        _hpController.OnGameOver += OnGameOver;
+        }
+
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                OnGameOver(0);
+            }
+            if (Input.GetKeyDown(KeyCode.B))
+            {
+                OnGameOver(1);
+            }
+            if (Input.GetKeyDown(KeyCode.C))
+            {
+                OnGameOver(2);
+            }
+        }
+
+        void OnGameOver(int c)
+        {
+            transform.GetChild(0).gameObject.SetActive(true);
+            CheckWin(c);
+        }
+        
+        void CheckWin(int c)
+        { 
+            switch (c)
+            {
+                case 0:
+                    _winner.text = "Congratulation \n Player 1 Wins"; 
+                    break;
+                case 1:
+                    _winner.text = "Congratulation \n Player 2 Wins";
+                    break;
+                case 2:
+                    _winner.text = "Too Bad No One Win";
+                    break;
+
+            }
+        }
+
+        void GoMainMenu() 
+        {
+            Debug.Log("MbOH");
+//            SceneManager.LoadScene("MainMenu")
+        }
+
+        void GoRetry() 
+        {
+            Debug.Log("Ya Ndak Tau Kok Tanya Saya");
+  //          Application.LoadLevel(Application.loadedLevel);
+        }
+    }
+}
