902bda84c319d5f9ceb6d56fd0ea9078a195a684
HiDE <msyariefhidayatulloh14@gmail.com>
2022-08-25 15:18:01 +0800

    Add Bomb Controller

---
+++ Assets/Scripts/Bombs/BomController.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TankU.Bomb
+{
+    public class BomController : MonoBehaviour
+    {
+        [SerializeField] private GameObject _bombPrefab;
+
+        private List<GameObject> _bombPool;
+
+        private void OnEnable()
+        {
+            
+        }
+
+        private void OnDisable()
+        {
+            
+        }
+
+        private void OnBombPlanted(Vector3 _plantedPosition)
+        {
+            GameObject _readyBomb = GetObjectFromPool(_bombPool);
+            if (_readyBomb == null)
+            {
+                _readyBomb = Instantiate(_bombPrefab, _plantedPosition, Quaternion.identity);
+                _readyBomb.SetActive(true);
+                _bombPool.Add(_readyBomb);
+            }
+            else
+            {
+                _readyBomb.transform.position = _plantedPosition;
+                _readyBomb.SetActive(true);
+            }
+        }
+
+        private GameObject GetObjectFromPool(List<GameObject> _pool)
+        {
+            return _pool.Find(go => !go.activeInHierarchy);
+        }
+    }
+}
+
