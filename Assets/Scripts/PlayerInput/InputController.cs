ea6d839868dae2e5725e212c50f021e9e5377127
azizichwani <ichwaniaziz@gmail.com>
2022-08-26 09:06:44 +0800

    Update input controller

--- Assets/Scripts/PlayerInput/InputController.cs
+++ Assets/Scripts/PlayerInput/InputController.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using TankU.HPSystem;
+using TankU.Timer;
 using UnityEngine;
 
 namespace TankU.PlayerInput
@@ -7,9 +9,36 @@ namespace TankU.PlayerInput
     public class InputController : MonoBehaviour
     {
         [SerializeField] private InputScriptable inputScriptable;
+        [SerializeField] private CountdownController countdownController;
+        [SerializeField] private HPController hpController;
+
+        private bool isGamePaused = true;
+
+        public void OnGameOver(int index)
+        {
+            
+        }
+
+        private void OnGameStarted()
+        {
+            isGamePaused = false;
+        }
+
+        private void OnEnable()
+        {
+            countdownController.OnCountdownEnded += OnGameStarted;
+            hpController.OnGameOver += OnGameOver;
+        }
+
+        private void OnDisable()
+        {
+            countdownController.OnCountdownEnded -= OnGameStarted;
+            hpController.OnGameOver -= OnGameOver;
+        }
 
         public Vector3 ProcessMoveInput()
         {
+            if (isGamePaused) return Vector3.zero;
             if (Input.GetKey(inputScriptable.InputKeys.moveLeft)) return Vector3.left;
             if (Input.GetKey(inputScriptable.InputKeys.moveRight)) return Vector3.right;
             if (Input.GetKey(inputScriptable.InputKeys.moveForward)) return Vector3.forward;
@@ -19,9 +48,15 @@ namespace TankU.PlayerInput
 
         public Vector3 ProcessRotateInput()
         {
+            if (isGamePaused) return Vector3.zero;
             if (Input.GetKey(inputScriptable.InputKeys.rotateLeft)) return new Vector3(0, -1, 0);
             if (Input.GetKey(inputScriptable.InputKeys.rotateRight)) return new Vector3(0, 1, 0);
             else return Vector3.zero;
+        }
+
+        private void Update()
+        {
+            
         }
     }
 }
