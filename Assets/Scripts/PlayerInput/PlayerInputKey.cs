8cc7acc86b7df540b56d586bd8eefb8bf51bfc08
azizichwani <ichwaniaziz@gmail.com>
2022-08-26 08:37:32 +0800

    add player modules

---
+++ Assets/Scripts/PlayerInput/PlayerInputKey.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace TankU.PlayerInput
+{
+    [System.Serializable]
+    public struct PlayerInputKey
+    {
+        [SerializeField] private KeyCode _moveForward;
+        [SerializeField] private KeyCode _moveBackward;
+        [SerializeField] private KeyCode _moveLeft;
+        [SerializeField] private KeyCode _moveRight;
+        [SerializeField] private KeyCode _rotateRight;
+        [SerializeField] private KeyCode _rotateLeft;
+
+        public KeyCode moveForward => _moveForward;
+        public KeyCode moveBackward => _moveBackward;
+        public KeyCode moveLeft => _moveLeft;
+        public KeyCode moveRight => _moveRight;
+        public KeyCode rotateRight => _rotateRight;
+        public KeyCode rotateLeft => _rotateLeft;
+    }
+}
+
