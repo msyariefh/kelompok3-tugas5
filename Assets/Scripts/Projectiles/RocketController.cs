28098eeb69b6ed9e82b6bb971c3a5e37fbfc2384
HiDE <msyariefhidayatulloh14@gmail.com>
2022-08-30 08:45:02 +0800

    Fix backwards projectile bug

--- Assets/Scripts/Projectiles/RocketController.cs
+++ Assets/Scripts/Projectiles/RocketController.cs
@@ -75,7 +75,6 @@ namespace TankU.Projectile
         void Start()
         {
             rb = GetComponent<Rigidbody>();
-            rb.velocity = speed * transform.forward;
         }
 
         private void OnTriggerEnter(Collider other)
